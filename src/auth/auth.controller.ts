da365d6d7f5f721da8c064406d3c4b03df147203
RashingPro <rashingpro@yandex.ru>
2025-04-07 12:01:46 +0300

    Improve token validation, add /auth/validate_token endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Post, Req, Res} from '@nestjs/common';
+import {Controller, Post, Req, Res} from '@nestjs/common';
 import {Request, Response} from "express";
 import {AuthService} from "./auth.service";
 
@@ -6,9 +6,19 @@ import {AuthService} from "./auth.service";
 export class AuthController {
     constructor(private authService: AuthService) {}
 
-    @Post('/decode_validate_token')
-    decodeValidateToken(@Req() req: Request, @Res() res: Response) {
-        console.log()
-        return res.status(200).json(req.cookies);
+    @Post('/validate_token')
+    validateToken(@Req() req: Request, @Res() res: Response) {
+        if (!(req["access_token"])) {
+            return res.status(401);
+        }
+        if (!process.env.DATABASE_URL) {
+            return res.status(500)
+        }
+        const token_validation = this.authService.validateToken(req["access_token"])
+        if (token_validation[0]) {
+            return res.status(200).json(JSON.stringify(token_validation[1]))
+        } else {
+            return res.status(401)
+        }
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import {Injectable} from "@nestjs/common";
-import jwt from "jsonwebtoken";
+import jwt, {JwtPayload} from "jsonwebtoken";
+import * as process from "node:process";
 
 @Injectable()
 export class AuthService {
+    private generateJwtId(userId: string): string {
+        return `${userId}`
+    }
     generateToken(payload: string, expiresIn: number, jwtId: string): string | null {
         if (!process.env.JWT_SECRET) {
             return null;
@@ -16,6 +20,26 @@ export class AuthService {
             return null;
         } catch (_) {
             return null;
+        }
+    }
+
+    validateToken(token: string): [boolean, JwtPayload | JSON | null] {
+        try {
+            if (!process.env.JWT_SECRET) throw new Error("Missing JWT_SECRET");
+            const _data = jwt.verify(token, `${process.env.JWT_SECRET}`)
+            let data;
+            if (typeof _data === "string") {
+                data = JSON.parse(data);
+            } else {
+                data = _data
+            }
+            const jwtId = this.generateJwtId(data["user"]["id"]);
+            data = jwt.verify(token, `${process.env.JWT_SECRET}`, {
+                jwtid: jwtId
+            })
+            return [true, data];
+        } catch (_) {
+            return [false, null];
         }
     }
 }
