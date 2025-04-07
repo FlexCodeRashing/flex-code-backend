538794b45c4c55397e10abad80133facf2ffc7b4
RashingPro <rashingpro@yandex.ru>
2025-04-07 12:43:14 +0300

    Move token validation to auth.guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,9 @@
-import {Controller, Get, Req, Res} from '@nestjs/common';
-import {Request, Response} from "express";
+import {Controller, Get, Req, Res, UseGuards} from '@nestjs/common';
 import {AuthService} from "./auth.service";
 
 @Controller('/auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
-    @Get('/validate_token')
-    validateToken(@Req() req: Request, @Res() res: Response) {
-        let token = req.headers.authorization;
-        if (!token || !token.startsWith("Bearer ")) {
-            return res.status(401);
-        }
-        token = token.replace('Bearer ', '');
-        if (!process.env.DATABASE_URL) {
-            return res.status(500)
-        }
-        const token_validation = this.authService.validateToken(token)
-        if (token_validation[0]) {
-            return res.status(200).json(JSON.stringify(token_validation[1]))
-        } else {
-            return res.status(401)
-        }
-    }
+
 }
